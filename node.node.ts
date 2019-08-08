a11e7fe44bcdb5bdcb7b8af3c3802b9d6304f4d5
КарловскийДА <kard@1c.ru>
2019-08-08 20:18:04 +0300

    Autoinstall node modules

--- node.node.ts
+++ node.node.ts
@@ -1,3 +1,9 @@
-var $node = new Proxy( {} , { get( target : any, field : string , wrapper : any ) {
-	return require( field )
+var $node = new Proxy( {} , { get( target : any, name : string , wrapper : any ) {
+	try {
+		require.resolve( name )
+	} catch( error ) {
+		if( error.code !== 'MODULE_NOT_FOUND' ) return $.$mol_fail_hidden( error )
+		$.$mol_exec( '.' , 'npm' , 'install' , name )
+	}
+	return require( name )
 } } )
