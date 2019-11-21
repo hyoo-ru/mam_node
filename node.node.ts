6bbb1466295493bda4eef1444e94960f937d6291
nin-jin <nin-jin@ya.ru>
2019-11-21 21:52:44 +0300

    Autoinstall types

--- node.node.ts
+++ node.node.ts
@@ -7,7 +7,7 @@ var $node = new Proxy( {} as any , { get( target , name : string , wrapper ) {
 	if( require( 'module' ).builtinModules.indexOf( name ) >= 0 ) return require( name )
 	
 	if( !require( 'fs' ).existsSync( `./node_modules/${ name }` ) ) {
-		$.$mol_exec( '.' , 'npm' , 'install' , name )
+		$.$mol_exec( '.' , 'npm' , 'install' , name , '@types/' + name )
 	}
 	
 	return require( name )
