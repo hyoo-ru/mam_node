cebd4237c43706529c7c6fdb4f01c26526a0a25a
nin-jin <nin-jin@ya.ru>
2019-11-21 21:59:50 +0300

    Ignore errors of types installing

--- node.node.ts
+++ node.node.ts
@@ -7,7 +7,13 @@ var $node = new Proxy( {} as any , { get( target , name : string , wrapper ) {
 	if( require( 'module' ).builtinModules.indexOf( name ) >= 0 ) return require( name )
 	
 	if( !require( 'fs' ).existsSync( `./node_modules/${ name }` ) ) {
-		$.$mol_exec( '.' , 'npm' , 'install' , name , '@types/' + name )
+
+		$.$mol_exec( '.' , 'npm' , 'install' , name )
+		
+		try {
+			$.$mol_exec( '.' , 'npm' , 'install' , '@types/' + name )
+		} catch {}
+		
 	}
 	
 	return require( name )
