f1aa8b0d80b90e38f77b177e9dd96f78afe489df
nin-jin <nin-jin@ya.ru>
2020-03-20 02:40:08 +0300

    Fixed types installing

--- node.node.ts
+++ node.node.ts
@@ -2,14 +2,27 @@ interface $node {
 	[key:string]: any
 }
 
+var _node_checked_types = new Set< string >()
+
 var $node = new Proxy( {} as any , { get( target , name : string , wrapper ) {
 
 	if( require( 'module' ).builtinModules.indexOf( name ) >= 0 ) return require( name )
-	
-	if( !require( 'fs' ).existsSync( `./node_modules/${ name }` ) ) {
+
+	const fs = require( 'fs' )
+	const deps = JSON.parse( fs.readFileSync( 'package.json' ) ).dependencies
+
+	if(!( name in deps )) {
+
 		$.$mol_exec( '.' , 'npm' , 'install' , name )
+
+		if( name[0] !== '@' ) {
+			try {
+				$.$mol_exec( '.' , 'npm' , 'install' , '@types/' + name )
+			} catch { }
+		}
+
 	}
-	
+
 	return require( name )
 
 } } ) as $node
