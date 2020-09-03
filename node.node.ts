af8b2de1909aa6659a9db9780010cdc9f54469b8
nin-jin <nin-jin@ya.ru>
2020-09-03 19:22:22 +0300

    require resources on nodejs support

--- node.node.ts
+++ node.node.ts
@@ -2,41 +2,62 @@ interface $node {
 	[key:string]: any
 }
 
-var $node = new Proxy( {} as any , { get( target , name : string , wrapper ) {
-
-	const path = require( 'path' ) as typeof import( 'path' )
-	const fs = require( 'fs' ) as typeof import( 'fs' )
-	const mod = require( 'module' ) as typeof import( 'module' )
-
-	if( mod.builtinModules.indexOf( name ) >= 0 ) return require( name )
-
-	let dir = path.resolve( '.' )
-	const suffix = `./node_modules/${ name }`
-
-	const $$ = ( $ as any )
+var $node = new Proxy( {} as any , {
 	
-	while( !fs.existsSync( path.join( dir , suffix ) ) ) {
+	get( target , name : string , wrapper ) {
 
-		const parent = path.resolve( dir , '..' )
+		if( target[ name ] ) return target[ name ]
 
-		if( parent === dir ) {
+		const mod = $node_require( 'module' ) as typeof import( 'module' )
+		
+		if( mod.builtinModules.indexOf( name ) >= 0 ) return $node_require( name )
+		
+		const path = $node_require( 'path' ) as typeof import( 'path' )
+		const fs = $node_require( 'fs' ) as typeof import( 'fs' )
 
-			$$.$mol_exec( '.' , 'npm' , 'install' , name )
-			
-			try {
-				$$.$mol_exec( '.' , 'npm' , 'install' , '@types/' + name )
-			} catch {}
+		let dir = path.resolve( '.' )
+		const suffix = `./node_modules/${ name }`
 
-			break
+		const $$ = ( $ as any )
+		
+		while( !fs.existsSync( path.join( dir , suffix ) ) ) {
 
-		} else {
+			const parent = path.resolve( dir , '..' )
 
-			dir = parent
+			if( parent === dir ) {
+
+				$$.$mol_exec( '.' , 'npm' , 'install' , name )
+				
+				try {
+					$$.$mol_exec( '.' , 'npm' , 'install' , '@types/' + name )
+				} catch {}
+
+				break
+
+			} else {
+
+				dir = parent
+
+			}
 
 		}
+		
+		return $node_require( name )
 
-	}
-	
-	return require( name )
+	},
 
-} } ) as $node
+	set( target , name : string , value ) {
+		target[ name ] = value
+		return true
+	},
+
+} ) as $node
+
+const $node_require = require
+
+require = ( req =>
+	Object.assign( function require( name : string ) {
+		return $node[ name ]
+	} , $node_require )
+)( require )
+
