21a766b8d012ff1376c8706a0a9060b3891c4c0d
jin <jin@hyoo.ru>
2021-03-23 12:35:17 +0300

    No require `require`.

--- node.node.ts
+++ node.node.ts
@@ -2,18 +2,18 @@ interface $node {
 	[key:string]: any
 }
 
-var $node = new Proxy( {} as any , {
+var $node = new Proxy( { require } as any , {
 	
 	get( target , name : string , wrapper ) {
 
 		if( target[ name ] ) return target[ name ]
 
-		const mod = $node_require( 'module' ) as typeof import( 'module' )
+		const mod = target.require( 'module' ) as typeof import( 'module' )
 		
-		if( mod.builtinModules.indexOf( name ) >= 0 ) return $node_require( name )
+		if( mod.builtinModules.indexOf( name ) >= 0 ) return target.require( name )
 		
-		const path = $node_require( 'path' ) as typeof import( 'path' )
-		const fs = $node_require( 'fs' ) as typeof import( 'fs' )
+		const path = target.require( 'path' ) as typeof import( 'path' )
+		const fs = target.require( 'fs' ) as typeof import( 'fs' )
 
 		let dir = path.resolve( '.' )
 		const suffix = `./node_modules/${ name }`
@@ -42,7 +42,7 @@ var $node = new Proxy( {} as any , {
 
 		}
 		
-		return $node_require( name )
+		return target.require( name )
 
 	},
 
@@ -53,11 +53,9 @@ var $node = new Proxy( {} as any , {
 
 } ) as $node
 
-const $node_require = require
-
 require = ( req =>
 	Object.assign( function require( name : string ) {
 		return $node[ name ]
-	} , $node_require )
+	} , req )
 )( require )
 
