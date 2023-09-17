9a53e13ffd94c9e92dde3a296e74a67cc552b0a8
jin <jin@hyoo.ru>
2023-09-17 16:24:18 +0300

    No no-save to prevent error on using on-the-fly installed deps

--- node.node.ts
+++ node.node.ts
@@ -27,10 +27,10 @@ var $node = new Proxy( { require } as any , {
 
 			if( parent === dir ) {
 
-				$$.$mol_exec( '.' , 'npm' , 'install' , '--omit=dev', '--no-save', name )
+				$$.$mol_exec( '.' , 'npm' , 'install' , '--omit=dev', name )
 				
 				try {
-					$$.$mol_exec( '.' , 'npm' , 'install' , '--omit=dev', '--no-save', '@types/' + name )
+					$$.$mol_exec( '.' , 'npm' , 'install' , '--omit=dev', '@types/' + name )
 				} catch {}
 
 				break
